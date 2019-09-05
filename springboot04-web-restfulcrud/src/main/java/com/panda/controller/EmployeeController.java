7c9af9557f73f9de1b1aea513a3d10264c85839e
likaisheng <1805318928@qq.com>
2019-09-05 22:09:33 +0800

    添加员工、修改员工、删除员工

--- springboot04-web-restfulcrud/src/main/java/com/panda/controller/EmployeeController.java
+++ springboot04-web-restfulcrud/src/main/java/com/panda/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.panda.controller;
 
+import com.panda.dao.DepartmentDao;
 import com.panda.dao.EmployeeDao;
+import com.panda.entities.Department;
 import com.panda.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -13,6 +15,8 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
 
     //查询所有员工返回列表页面
     @GetMapping("/emps")
@@ -22,5 +26,49 @@ public class EmployeeController {
         model.addAttribute("emps",employees);
         //thymeleaf默认就会拼串  classpath:/templates/xxx.html
         return "emp/list";
+    }
+    //来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+    //员工添加
+    //springmvc自动将请求参数和入参对象的属性进行一一绑定，
+    //要求：请求参数的名字和javaBean入参的对象里面的属性名是一致的
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //来到员工列表页面
+        System.out.println("保存的员工信息:"+employee);
+        employeeDao.save(employee);
+        //redirect:表示重定向到一个地址  /代表当前项目路径
+        //forward:表示转发到一个地址
+        return "redirect:/emps";
+    }
+    //来到修改页面，查出当前员工，在页面回显
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id")Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        //add是一个修改添加二合一的页面
+        return "emp/add";
+    }
+    //修改员工:需要提交员工id
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee){
+        System.out.println("修改的员工数据：" + employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmployee(@PathVariable("id")Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
     }
 }
